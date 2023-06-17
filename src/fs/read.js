256f4489384e97df42bf14dd9d131cbcc0dd3853
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-17 14:57:32 +0400

    feat: add solution for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import fs from 'fs';
+import { access, constants } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const pathFile = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
+  access(pathFile, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed: file fileToRead.txt does not exist');
+    } else {
+      fs.readFile(pathFile, { withFileTypes: true }, (readErr, content) => {
+        if (readErr) {
+          throw new Error(`Error reading directory: ${readErr}`);
+        }
+        console.log(content.toString());
+      });
+    }
+  });
 };
 
 await read();
