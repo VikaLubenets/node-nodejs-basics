a6878eba1be9252837f50f397870b76099dd9c56
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-17 13:44:47 +0400

    feat: add copy.js solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,49 @@
+import fs from 'fs';
+import { access, constants } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const pathFolderCurr = path.resolve(__dirname, 'files');
+    const pathFolderTarget = path.resolve(__dirname, 'files-copy');
+
+    access(pathFolderCurr, constants.F_OK, (err) => {
+        if (err){
+            
+            console.log('FS operation failed: folder files does not exist');
+
+        } else {
+            access(pathFolderTarget, constants.F_OK, (err) => {
+                if (err) {
+                    fs.mkdir(pathFolderTarget, err => {
+                        if (err) throw err;
+                        console.log('Folder created successfully');
+                    });
+                    
+                    fs.readdir(pathFolderCurr, { withFileTypes: true }, (err, files) => {
+                        if (err) {
+                        return console.log(`Error reading directory: ${err}`);
+                        }
+                        files.forEach((file) => {
+                        const pathSource = path.join(pathFolderCurr, file.name);
+                        const pathTarget = path.join(pathFolderTarget, file.name);
+                        if (file.isFile()) {
+                            fs.copyFile(pathSource, pathTarget, (err) => {
+                            if (err) throw err;
+                            });
+                        }
+                        });
+                    });
+                } else {
+                  console.log('FS operation failed: folder files-copy already exists');
+                }
+              });
+        }
+    })
+   
+
 };
 
 await copy();
