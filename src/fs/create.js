6367bb2fb002cf21b431bbd8a32bba43dbdc5594
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-17 13:17:10 +0400

    feat: add create.js solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { writeFile } from 'fs';
+import { access, constants } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
-};
+    const file = path.join(__dirname, 'files', 'fresh.txt');
+
+    access(file, constants.F_OK, (err) => {
+      if (err) {
+        writeFile(file, 'I am fresh and young', (writeErr) => {
+          if (writeErr) throw writeErr;
+          console.log('File created successfully');
+        });
+      } else {
+        console.log('FS operation failed: File already exists');
+      }
+    });
+}
 
 await create();
+
+
+
+
+
+
