17ce6fad88cab5ef465cdc7e95d9a34bcb8edb4a
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-17 16:45:19 +0400

    feat: add solution for args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const strArr = process.argv;
+    const obj = {};
+  
+    for (let i = 0; i < strArr.length; i += 2) {
+      const name = strArr[i].slice(2);
+      const value = strArr[i + 1];
+      obj[name] = value;
+    }
+  
+    for (const prop in obj) {
+      console.log(`${prop} is ${obj[prop]}`);
+    }
 };
 
 parseArgs();
