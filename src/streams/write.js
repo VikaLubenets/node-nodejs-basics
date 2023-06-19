3e6c6048eb8e5647bce049bcd09f29a9cac761d8
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-19 17:28:02 +0400

    feat: add solution for write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,30 @@
+import readline from 'readline';
+import path from 'path';
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);  
+
 const write = async () => {
-    // Write your code here 
+    const readLine = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    const file = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const stream = createWriteStream(file, { flags: 'a', encoding: 'utf-8' });
+
+    console.log('Введите, пожалуйста, сообщение, которое нужно записать в файл fileToWrite.txt. Если хотите завершить ввод, введите close');
+
+    readLine.on('line', (input) => {
+        if(input === 'close'){
+            console.log('Выполнение программы завершено');
+            stream.end();
+            process.exit();
+        }
+        stream.write(`${input}\n`);
+    });
+
 };
 
 await write();
