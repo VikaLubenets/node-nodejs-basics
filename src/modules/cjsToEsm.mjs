4abf2ff87cc20fe63649d4779b0791a1ce42eb01
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-19 16:07:24 +0400

    feat: add solution for cjsToEsm.mjs

--- src/modules/cjsToEsm.mjs
+++ src/modules/cjsToEsm.mjs
@@ -1,16 +1,22 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c';
+import jsonA from './files/a.json';
+import jsonB from './files/b.json';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = jsonA;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = jsonB;
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +39,5 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+export { unknownObject, myServer };
 
