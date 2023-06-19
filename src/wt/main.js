f5f61cc15991f810fb3e374dce5c0dea21d0feca
Vika Lubenets <lubenetcvika@gmail.com>
2023-06-19 22:48:38 +0400

    feat: add solution for worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,7 +1,9 @@
 import { Worker, isMainThread, parentPort, workerData } from 'node:worker_threads';
 
 const performCalculations = async () => {
-    // Write your code here
+    
+    
+    
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,10 +1,25 @@
 import { Worker, isMainThread, parentPort, workerData } from 'node:worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);  
 
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (n) => {
+    if (isMainThread) {
+        const worker = new Worker(__filename, { workerData: n });
+        worker.on('message', (result) => {
+          console.log('Result received:', result); 
+        });
+    } else {
+        const result = nthFibonacci(workerData);
+        parentPort.postMessage(result);
+    }
 };
 
-sendResult();
+
+const n = 7;
+sendResult(n);
