509748e07d58099ca8131a2dc9d159342c7dcde1
VikaLubenets <lubenetcvika@gmail.com>
2024-10-03 16:43:09 +0200

    feat: add solution for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,7 +1,39 @@
-import { Worker, isMainThread, parentPort, workerData } from 'node:worker_threads';
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const performCalculations = async () => {
-    //code here
+    const quantatyCPU = cpus().length;
+    let n = 10;
+    const results = new Array(quantatyCPU).fill(null);
+    const workers = [];
+
+    for (let i = 0; i < quantatyCPU; i++) {
+        workers.push(
+            new Promise((resolve, reject) => {
+                const worker = new Worker(join(__dirname, 'worker.js'), { workerData: { n } });
+                n++;
+
+                worker.on('message', (result) => {
+                    results[i] = { status: 'resolved', data: result };
+                    resolve();
+                });
+
+                worker.on('error', (err) => {
+                    results[i] = { status: 'error', data: null };
+                    reject(err);
+                });
+            })
+        );
+    }
+
+    await Promise.all(workers);
+
+    console.log(results);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,25 +1,10 @@
-import { Worker, isMainThread, parentPort, workerData } from 'node:worker_threads';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);  
+import { parentPort, workerData } from 'node:worker_threads';
 
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = (n) => {
-    if (isMainThread) {
-        const worker = new Worker(__filename, { workerData: n });
-        worker.on('message', (result) => {
-          console.log('Result received:', result); 
-        });
-    } else {
-        const result = nthFibonacci(workerData);
-        parentPort.postMessage(result);
-    }
+const sendResult = () => {
+    parentPort.postMessage(nthFibonacci(workerData.n))
 };
 
-
-const n = 7; //example, you can change n for tests
-sendResult(n);
+sendResult();
